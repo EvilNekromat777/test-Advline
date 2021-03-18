770144e9e983e35813a97521d596cfd23a668dfd
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-03-18 13:03:29 +0300

    feat: scripts

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ var myCarousel = document.querySelector('#carouselExampleFade')
 var carousel = new bootstrap.Carousel(myCarousel, { 
   interval: false
 });
+
+//======================================>
+$('.carousel-indicators__orange').click(function(){
+	$('.svg, .link, .wrapper').removeClass('active');
+	$('.svg__orange').addClass('active');
+});
+$('.carousel-indicators__green').click(function(){
+	$('.svg, .link, .wrapper').removeClass('active');
+	$('.svg__green').addClass('active');
+});
+$('.carousel-indicators__yellow').click(function(){
+	$('.svg').removeClass('active');
+	$('.svg__yellow, .wrapper, .link').addClass('active');
+});
+$('.carousel-indicators__lightGreen').click(function(){
+	$('.svg, .link, .wrapper').removeClass('active');
+	$('.svg__lightGreen').addClass('active');
+});
+
